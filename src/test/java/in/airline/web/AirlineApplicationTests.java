ae51d12349117ab01f0220d93174380f4d9a3c8b
unknown <spider8019official@gmail.com>
2023-11-02 11:21:15 +0530

    Initializing the airline repo

---
+++ src/test/java/in/airline/web/AirlineApplicationTests.java
@@ -0,0 +1,13 @@
+package in.airline.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
